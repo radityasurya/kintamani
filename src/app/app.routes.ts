f0827477ac1cae8c9d704358b25f0f6b7fc87d9b
radityasurya <radityasurya@hotmail.com>
2016-12-28 14:03:21 +0100

    add web routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+ export const AppRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'work', component: WorkComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: HomeComponent }
+
+];
